7db75d679426918690c8aef13a24da1424fba145
Can ŞAFAK <safaksafakcan@gmail.com>
2024-04-26 14:40:36 +0300

    UserService'nin UserController'a inject edilmesi.

--- src/users/controllers/user/user.controller.ts
+++ src/users/controllers/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { UserService } from 'src/users/services/user/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    @Inject('USER_SERVICE') private readonly userService: UserService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.userService.getHello();
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './controllers/users/users.controller';
 import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user/user.service';
 
 @Module({
-  controllers: [UsersController, UserController],
-  providers: [UserService]
+  controllers: [UserController],
+  providers: [
+    {
+      provide: 'USER_SERVICE',
+      useClass: UserService,
+    },
+  ],
 })
 export class UsersModule {}
